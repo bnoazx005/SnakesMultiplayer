54ae16ad6e113f4cab9b26b6569182766282add7
bnoazx005 <ildar2571@yandex.ru>
2017-11-05 13:20:49 +0400

    Add initialization code into Game class
    Changes:
    * An initialization code was implemented for Game class.

--- application/modules/game/game.js
+++ application/modules/game/game.js
@@ -1,7 +1,46 @@
 var GameStructures = require("./gameStructures");
 var Vector2D = require("./../math/vector2d");
 
-function Game() {
+
+function Game(origin, sizes, initialAmountOfFood) {
+	var mScene   = null;
+	var mPlayers = null;
+	var mFood    = null;
+
+	this.AddPlayer = function(color, name) {
+		mPlayers.push(new GameStructures.Player([], GameStructures.MOVE_DIRECTIONS.RIGHT, color, name));
+	};
+
+	this.Update = function() {
+	};
+
+	var _initFoodArray = function(sceneInstance, maxNumOfEntities) {
+		var foodArray = [];
+
+		var pos = null;
+
+		var halfSizes = Vector2D.Mul(sceneInstance.mSizes, 0.5);
+
+		//this algorithm doesn't check up food overlapping
+		for (var i = 0; i < maxNumOfEntities; ++i) {
+			pos = new Vector2D(Math.floor(Math.random() * sceneInstance.mSizes.x) - halfSizes.x,
+							   Math.floor(Math.random() * sceneInstance.mSizes.y) - halfSizes.y);
+
+			foodArray[i] = new GameStructures.Food(pos);
+		}
+
+		return foodArray;
+	};
+
+	function _init() {
+		mScene = new GameStructures.Scene(origin, sizes);
+
+		mFood = _initFoodArray(mScene, initialAmountOfFood || 20);
+
+		mPlayers = [];
+	}
+
+	_init();
 }
 
 module.exports = Game;
