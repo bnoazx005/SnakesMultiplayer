071e6530b5de7e4f9ac8f4020e80027549c1d76d
bnoazx005 <ildar2571@yandex.ru>
2017-12-07 22:01:54 +0400

    Fix a bug of incorrect key codes usage
    Changes:
    * Zooming didn't work in Chrome, because of difference in key codes
    comparing to Firefox. Now it was fixed.

--- public/js/config.js
+++ public/js/config.js
@@ -6,8 +6,8 @@ var Config = (function() {
 		UP       : 38,
 		RIGHT    : 39,
 		DOWN     : 40,
-		ZOOM_IN  : 61,
-		ZOOM_OUT : 173
+		ZOOM_IN  : navigator.userAgent.search("Firefox") > -1 ? 61 : 187, //should be tested more
+		ZOOM_OUT : navigator.userAgent.search("Firefox") > -1 ? 173 : 189 //and why the browsers use different key codes?!
 	};
 
 	mExports.ZoomStep = 5;
